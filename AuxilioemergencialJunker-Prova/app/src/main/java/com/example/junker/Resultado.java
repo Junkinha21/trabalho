465bd4a220f1c4bc5da501ea7a4a4dffdb260979
Your Name <lucas.rus@edu.unipar.br>
2020-06-28 23:59:21 -0300

    professor faz prova mais fácil por favor

---
+++ AuxilioemergencialJunker-Prova/app/src/main/java/com/example/junker/Resultado.java
@@ -0,0 +1,42 @@
+package com.example.junker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Resultado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        setData();
+    }
+
+    private void setData(){
+        try {
+            Calendar cal = Calendar.getInstance();
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+            Bundle b = getIntent().getExtras();
+            double saldo = b.getDouble("saldo");
+            cal.setTime(df.parse(b.getString("data")));
+            String date = cal.get(Calendar.DAY_OF_MONTH)+"/"+(cal.get(Calendar.MONTH)+1)+"/"+cal.get(Calendar.YEAR);
+            TextView txtSaldo = (TextView) findViewById(R.id.txtSaldo);
+            TextView txtDate = (TextView) findViewById(R.id.txtPayDate);
+
+            txtSaldo.setText(saldo + "Reais ");
+            txtDate.setText(date);
+        }catch (ParseException pe){
+            Toast toast = Toast.makeText(getApplicationContext(), "Aconteceu um erro", Toast.LENGTH_LONG);
+            toast.setMargin(0,0);
+            toast.show();
+        }
+
+    }
+}
